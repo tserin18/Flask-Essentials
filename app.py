ddf8f81cdf53a2bc1ec90355b57ba3c451e837bd
tserin18 <sherpa.tsering2011@gmail.com>
2018-05-09 12:37:13 -0700

    Initial working commit

---
+++ app.py
@@ -0,0 +1,55 @@
+#################################################
+# Import Dependencies
+# @TODO: Import necessary libraries
+#################################################
+
+from flask import Flask, render_template
+import scrape_mars  
+from pymongo import MongoClient
+
+#################################################
+# Flask Setup
+# @TODO: Initialize your Flask app here
+#################################################
+app = Flask(__name__)
+
+#################################################
+# MongoDB Setup
+# @TODO: Initialize your MongoDB app here
+#################################################
+
+client = MongoClient("mongodb://admin:Namaste100@ec2-18-144-8-16.us-west-1.compute.amazonaws.com:27017")
+db = client.mission_to_mars
+
+#################################################
+# MongoDB Mnipulation
+# @TODO: Initialize your MongoDB app here
+#################################################
+
+
+#################################################
+# Flask Routes
+# @TODO:  Create a route and view function that takes in a string and renders index.html template
+#################################################
+@app.route("/")
+def index():
+    new_data = db.general.find_one()
+    for k, v in new_data.items(): 
+        if k == "news":
+            news = v
+        elif k == "weather":
+            weather = v
+        elif k == "featured_image_url":
+            featured_image_url = v
+        """ elif k == "hemisphere_image_urls":
+            hemisphere_image_urls = v 
+        
+        elif k == "facts":
+            facts = v """
+        
+    #db.general.insert_one(output) 
+    return render_template('index.html',news=news,weather=weather,featured_image_url=featured_image_url)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,105 @@
+import time
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+
+def init_browser():
+     # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/Users/tseringsherpa/Desktop/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+    
+def get_news(browser):
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    try:
+        browser.visit(url)
+        html_string = browser.html
+        soup = bs(html_string, 'html.parser')
+
+        div = soup.find('div', attrs={'class': 'list_text'})
+        title=div.findNext('div', {'class': 'content_title'}).text            
+        description=div.findNext('div', {'class': 'article_teaser_body'}).text
+    except:
+        pass
+    return {"news_title":title,"news_p":description}
+
+def get_featured_image(browser):
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    try:
+        browser.visit(url)
+        button = browser.find_by_id("full_image")
+        button.click()
+        time.sleep(2)
+
+        html_string = browser.html
+        soup = bs(html_string, 'html.parser')
+        anchor = soup.find('a','ready')
+        if anchor.img:
+            image_url = anchor.img['src']
+        featured_image_url = "https://www.jpl.nasa.gov" + image_url      
+    except:
+        pass
+    return featured_image_url
+
+def get_latest_weather(browser):
+    url = 'https://twitter.com/marswxreport?lang=en'
+    try:
+        browser.visit(url)
+        html_string = browser.html
+        soup = bs(html_string, 'lxml')
+        
+        latest_weather = soup.find('div','js-tweet-text-container').text.strip()
+    except:
+        pass
+    return latest_weather 
+
+def get_facts(browser):
+    url = 'https://space-facts.com/mars/'
+    try:
+        browser.visit(url)
+        html_string = browser.html
+        soup = bs(html_string, 'lxml')
+
+        keys =[]
+        values=[]
+        table = soup.find('table','tablepress tablepress-id-mars')
+        for row in table.find_all('tr'):
+            columns = row.find_all('td')
+            keys.append(columns[0].text)
+            values.append(columns[1].text)
+            facts = dict(zip(keys, values))
+    except:
+        pass
+    return facts
+
+def get_hemispheres(browser):
+    hemisphere_image_urls = []
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars' 
+    try:
+        browser.visit(url)     
+        html_string = browser.html
+        soup = bs(html_string, 'lxml')
+
+        for header in soup.find_all("h3"):
+            title = header.text
+            uri = header.find_previous("a")
+            image_url = 'https://astrogeology.usgs.gov'+ uri['href'] 
+            browser.visit(image_url)
+
+            sub_html_string = browser.html
+            sub_soup = bs(sub_html_string, 'lxml')
+            image_url='https://astrogeology.usgs.gov' + str(sub_soup.find('img','wide-image')['src'])
+            hemisphere_image_urls.append({"title": title, "img_url": image_url})
+            browser.back()
+    except:
+        pass
+    return hemisphere_image_urls
+
+def scrape():
+    browser = init_browser()
+    output ={}
+    news =get_news(browser)
+    featured_image_url= get_featured_image(browser)
+    latest_weather=get_latest_weather(browser)
+    facts =get_facts(browser)
+    hemisphere_image_urls =get_hemispheres(browser)
+    output ={ "news":news,"featured_image_url":featured_image_url,"weather":latest_weather,"facts":facts, "hemisphere_image_urls":hemisphere_image_urls}
+    return output
